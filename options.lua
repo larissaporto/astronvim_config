74eeee93525ad090ef5323035521891613c404d1
Larissa Porto <larissa.silva86@gmail.com>
2023-08-31 22:29:39 +0200

    added the font for the neovide

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    guifont = "MesloLGS Nerd Font:h14" 
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
