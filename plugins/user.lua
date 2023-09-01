13a7424374af098bcea5ee7bbae2f24874b0b865
Larissa Silva <larissa.silva@xing.com>
2023-09-01 16:07:19 +0200

    added nvim-surround to add brackets and etc...

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,11 @@
 return {
+   -- Surround functionality
+     'kylechui/nvim-surround',
+     version = '*',
+     event = "VeryLazy",
+     config = function()
+       require('nvim-surround').setup()
+      end
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
