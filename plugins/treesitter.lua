399db2fd29b9ac6463e56000ce6e513ab01207ba
Larissa Silva <larissa.silva@xing.com>
2023-09-07 14:49:27 +0200

    add graphql to treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
       "luap",
-      "ruby"
+      "ruby",
+      "graphql"
     })
   end,
 }
