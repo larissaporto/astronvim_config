20ab42aa162d837e615510d12cc240c1e8ece74d
Larissa Silva <larissa.silva@xing.com>
2024-01-24 13:39:06 +0100

    Add option to remember windows size in neovide

--- init.lua
+++ init.lua
@@ -69,6 +69,10 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    if vim.g.neovide then
+      vim.g.neovide_remember_window_size = true
+    end
+
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
