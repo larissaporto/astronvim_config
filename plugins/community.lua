5cfaaa0c095087fa6db069d1a71661264e93776b
Larissa Silva <larissa.silva@xing.com>
2024-02-26 09:47:32 +0100

    Add copilot to astronvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,9 +3,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.motion.mini-move" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
---
+++ plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+-- "github/copilot.vim",
+--  event = "InsertEnter",
+--  autoStart = true,
+--  config = function ()
+--    vim.cmd "source ~/.config/nvim/extra_conf.vim"
+--  end
+}
