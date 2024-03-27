5e6d57bdebfa491f910e8edf4d2beff53ebd495a
Larissa Silva <larissa.silva@xing.com>
2024-03-27 17:32:09 +0100

    Remove the swapfiles

--- options.lua
+++ options.lua
@@ -7,7 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
-    guifont = "MesloLGS Nerd Font:h14"
+    guifont = "MesloLGS Nerd Font:h14",
+    swapfile = false,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
