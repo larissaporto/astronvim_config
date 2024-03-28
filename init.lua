03ed02778f52a96f1f1cf58901c7e05c7379344e
Larissa Silva <larissa.silva@xing.com>
2024-03-28 16:41:27 +0100

    Make catppuccin macchiato the default one

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "catppuccin-macchiato",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
