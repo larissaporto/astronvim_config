e44fa9b278e281b3fbd5d6190e84200e6e33d70e
Larissa Silva <larissa.silva@xing.com>
2024-03-28 17:11:50 +0100

    Add description comments

--- init.lua
+++ init.lua
@@ -69,16 +69,19 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    -- Trying to keep the neovide rememeber window size setting
     if vim.g.neovide then
       vim.g.neovide_remember_window_size = true
     end
 
+    -- Remove trailing whitespace on save... so not to dependend on linting
     vim.api.nvim_create_autocmd ('BufWritePre',{
       pattern = "*", -- run on all filetypes
       command = [[%s/\s\+$//e]], -- remove trailing whitespace
       desc = "remove trailing whitespace",
     })
 
+    -- Set up spec opener for Ruby files
     local setup_spec_opener = function()
       vim.keymap.set(
         'n',
@@ -105,12 +108,14 @@ return {
       desc = 'Setup spec opener'
     })
 
+    -- Set up Telescope live grep for selected word: better <leader>fc from astro
     vim.keymap.set('v', '<leader>f', function()
       vim.api.nvim_input('y')
       vim.api.nvim_input('<cmd> Telescope live_grep <CR>')
       vim.api.nvim_input('<c-r>')
       vim.api.nvim_input('0')
     end, { desc = 'Telescope live grep for selection' })
+
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
