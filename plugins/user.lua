c5c9841b8a1ceca9a64e8bb60c006c15f190a50a
Larissa Silva <larissa.silva@xing.com>
2024-03-28 16:20:56 +0100

    Add nv chad theme clone

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,18 @@
 return {
-   -- Surround functionality
-     'kylechui/nvim-surround',
-     version = '*',
-     event = "VeryLazy",
-     config = function()
-       require('nvim-surround').setup()
-      end,
+  {
+    -- Surround functionality
+    'kylechui/nvim-surround',
+    version = '*',
+    event = "VeryLazy",
+    config = function()
+         require('nvim-surround').setup()
+    end,
+  },
 
+  {
+    "aikow/base.nvim",
+    opts = {},
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
